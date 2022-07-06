8e269aec0cb3ba59ffb109e305155ae29ba9347d
lacz <lukasz@diagmol.com>
2022-07-06 16:05:56 +0200

    Update scrypt_aligment_end.sh

--- scrypt_aligment_end.sh
+++ scrypt_aligment_end.sh
@@ -17,13 +17,16 @@ do
 	echo "${run1[$i]}"
 	echo "${run2[$i]}"
 	name=${run1[$i]%%_*}
-	#fastqc ${run1[$i]} -o fastqc/
-	#fastqc ${run2[$i]} -o fastqc/
+	fastqc ${run1[$i]} -o fastqc/
+	fastqc ${run2[$i]} -o fastqc/
 	hisat2 -p 8 -x ../../ref/hg38/HG38.fna -1 ${run1[$i]} -2 ${run2[$i]} -S sam3/$name".sam"
 	samtools sort sam3/$name".sam" > bam3/$name".bam"
 	samtools index bam3/$name".bam"
 	i=$[i+1]
-	python ~/REDItools/main/REDItoolDnaRna.py -i bam3/$name".bam" -f ~/Documents/\!NGS_obrobka/ref/hg38/HG38.fna -n 6 -t 12 -o $name
+	python ~/REDItools/main/REDItoolDnaRna.py -i bam3/$name".bam" -f ../../!NGS_obrobka/ref/hg38/HG38.fna -n 6 -t 12 -o $name
 done
+echo "differential expression"
+featureCounts -g gene_name -a $ref.”/Homo_sapiens.GRCh38.96.chr.gtf “-o counts.txt bam/*.bam
+cat counts.txt | Rscript ../../\!skrypty/edger.r 2x4 > edger-results.csv
 echo "end script"
 
